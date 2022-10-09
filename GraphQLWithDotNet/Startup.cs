9c38631164e23f6b339a87c0f0eb57acc6116a4b
Kenneth <Kenneth.p.huang@gmail.com>
2022-10-09 19:35:45 +1000

    init—create model, schema, type, query and mutation for product

--- GraphQLWithDotNet/Startup.cs
+++ GraphQLWithDotNet/Startup.cs
@@ -10,6 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using GraphQLWithDotNet.Services;
+using GraphQLWithDotNet.Interfaces;
+using GraphQLWithDotNet.Type;
+using GraphQLWithDotNet.Query;
+using GraphQLWithDotNet.Schema;
+using GraphQL.Types;
+using GraphQL.Server;
+using GraphiQl;
 
 namespace GraphQLWithDotNet
 {
@@ -26,6 +34,16 @@ namespace GraphQLWithDotNet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<IProduct, ProductService>();
+            services.AddSingleton<ProductType>();
+            services.AddSingleton<ProductQuery>();
+            services.AddSingleton<ISchema, ProductSchema>();
+
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = false;
+            }).AddSystemTextJson();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,16 +54,18 @@ namespace GraphQLWithDotNet
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+            app.UseGraphiQl("/graphql");
+            app.UseGraphQL<ISchema>();
         }
     }
 }
